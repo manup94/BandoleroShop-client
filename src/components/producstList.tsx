c9d4726f190acac73755d67fc2f132572cae74c1
Manuel Pérez <mperezpradosjose@gmail.com>
2023-08-10 00:37:56 +0200

    done

--- src/components/producstList.tsx
+++ src/components/producstList.tsx
@@ -5,7 +5,7 @@ import ProductCard from "./productCard"
 function productsList({ products }: { products: Product[] }) {
 
     return (
-        <ul className="grid grid-cols-3 gap-4">
+        <ul className="grid grid-cols-3  gap-4">
             {
                 products?.map((product: Product) => {
                     return <ProductCard key={product.id} product={product} />
