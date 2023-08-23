7b3334adebeca8418acdad0b6f89d4a52584f353
Manuel Pérez <mperezpradosjose@gmail.com>
2023-08-23 14:28:02 +0200

    env solved

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-    SERVER_HOST: 'http://localhost:1337',
-    API_URL: 'http://localhost:1337/api',
+    SERVER_HOST: 'https://strapi-ktbr.onrender.com',
+    API_URL: 'https://strapi-ktbr.onrender.com/api',
     ENDPOINTS: {
         AUTH: {
             REGISTER: 'auth/local/register',
