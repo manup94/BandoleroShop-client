3629e5b319fed8bbda03b080058ff5f71154c312
Manuel Pérez <mperezpradosjose@gmail.com>
2023-08-23 16:29:08 +0200

    solved

--- src/components/auth/dialog.tsx
+++ src/components/auth/dialog.tsx
@@ -2,7 +2,7 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { useState } from 'react';
 import { LoginForm } from './login';
 import { SignUpForm } from './signup';
-import Image from 'next/image';
+
 
 
 function AuthDialog() {
@@ -32,7 +32,7 @@ function AuthDialog() {
                 <Dialog.Overlay onClick={closeDialog} className="bg-black/50 data-[state=open]:animate-overlayShow fixed inset-0" />
                 <Dialog.Content className="overflow-auto data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[600px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                     <div className='justify-center flex' >
-                        <img src={'/../public/images/logo.png'} alt='logo' className='w-1/4'></img>
+                        <img src={'/images/logo-copia.png'} className='w-50' alt='logo' />
                     </div>
                     {mode === 'login' && <LoginForm onSignUp={() => setMode('signup')} onLoginSuccess={handleLoginSuccess} />}
                     {mode === 'signup' && <SignUpForm onOpen={() => setOpen(false)} onLogin={closeDialog} />}
